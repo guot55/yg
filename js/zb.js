21c933fc851587111185f2be90eb12726aaf67fa
guoting <guot55@163.com>
2024-01-21 07:56:40 +0000

    j
    Signed-off-by: guoting <guot55@163.com>

---
+++ js/zb.js
@@ -0,0 +1,6 @@
+[
+
+{"name": "宝盒直播",     "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/ygzb.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+ {"name": "MV合集",     "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/mv.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"},
+ {"name": "MTV合集",     "url": "https://qu.ax/KmzB.txt&&&https://agit.ai/guot54/ygbh/raw/branch/master/yg.jpg"}
+]
